a52419e21f092eeafa4996895e95708bcafb7319
wuwenhui <2286785017@qq.com>
2020-03-16 21:58:07 +0800

    'mylayout第一次'

---
+++ src/router/index.js
@@ -0,0 +1,48 @@
+// 路由器对象模块
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '@/pages/Home/Home.vue'
+import Order from '@/pages/Order/Order.vue'
+import Profile from '@/pages/Profile/Profile.vue'
+import Search from '@/pages/Search/Search.vue'
+import Login from '@/pages/Login/Login.vue'
+//声明使用插件
+Vue.use(VueRouter)
+
+export default new VueRouter({
+    //所有路由
+    routes:[
+        {
+            path:'/home',
+            component:Home,
+            meta:{
+                showFooter: true
+            }
+        },
+        {
+            path:'/order',
+            component:Order,
+            meta:{
+                showFooter: true
+            }
+        },
+        {
+            path:'/profile',
+            component:Profile,
+            meta:{
+                showFooter: true
+            }
+        },
+        {
+            path:'/search',
+            component:Search,
+            meta:{
+                showFooter: true
+            }
+        },
+        {
+            path:'/login',
+            component:Login,
+        }
+    ]
+})
